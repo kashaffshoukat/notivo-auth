d51b21e9e75879dcce0b2970ddb3bf7130e40f7c
kashaf shoukat <139368562+kashaffshoukat@users.noreply.github.com>
2025-08-11 21:49:35 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SigninPage from "./pages/SigninPage";
 import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/signup" replace />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<SigninPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   );
 }
